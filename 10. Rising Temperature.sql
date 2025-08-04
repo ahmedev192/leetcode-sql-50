175c154b3ee40cab353c99985ea04462be22e29a
Ahmed Mahmoud <ahmedev192@gmail.com>
2025-08-04 06:51:32 +0300

    Rising Temperature

--- 10. Rising Temperature.sql
+++ 10. Rising Temperature.sql
@@ -1,2 +1,7 @@
 -- 10. Rising Temperature
--- Question goes here.
+/* Write your T-SQL query statement below */
+select w1.id 
+from weather w1 
+left join weather w2 
+on w2.recordDate = DATEADD(day, -1, w1.recordDate)
+where w1.temperature  > w2.temperature ;
