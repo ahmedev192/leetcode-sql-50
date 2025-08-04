f86f63dee0d030048e6d052b2c75cc94f8935272
Ahmed Mahmoud <ahmedev192@gmail.com>
2025-08-04 09:31:08 +0300

    Managers with at Least 5 Direct Reports

--- 13. Managers with at Least 5 Direct Reports.sql
+++ 13. Managers with at Least 5 Direct Reports.sql
@@ -1,2 +1,10 @@
 -- 13. Managers with at Least 5 Direct Reports
--- Question goes here.
+SELECT e.name
+FROM Employee e
+JOIN (
+    SELECT managerId
+    FROM Employee
+    WHERE managerId IS NOT NULL
+    GROUP BY managerId
+    HAVING COUNT(*) >= 5
+) m ON e.id = m.managerId;
