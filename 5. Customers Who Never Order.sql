e2de26e3517ea9b914b179ceae3fb289013aa113
Ahmed Mahmoud <ahmedev192@gmail.com>
2025-08-04 06:33:12 +0300

    Customers Who Never Order

--- 5. Customers Who Never Order.sql
+++ 5. Customers Who Never Order.sql
@@ -1,2 +1,7 @@
 -- 5. Customers Who Never Order
--- Question goes here.
+select v.customer_id , Count(*) AS count_no_trans
+from visits  v
+left join transactions  t
+on v.visit_id = t.visit_id 
+where transaction_id is null
+group by v.customer_id ; 
