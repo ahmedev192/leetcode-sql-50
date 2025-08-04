5bcd175f6540d98b4597cfd450a427831973cb39
Ahmed Mahmoud <ahmedev192@gmail.com>
2025-08-04 08:30:22 +0300

    Employee Bonus

--- 54. Replace Employee ID With The Unique Identifier.sql
+++ 54. Replace Employee ID With The Unique Identifier.sql
@@ -1,2 +1,1 @@
-/* Replace Employee ID With The Unique Identifier */
 select eu.unique_id , e.name   from Employees as e left join EmployeeUNI as eu on e.id = eu.id;
---
+++ 55. Employee Bonus.sql
@@ -0,0 +1,6 @@
+/* Employee Bonus */
+select e.name , bonus 
+from employee e 
+left join bonus b 
+on e.empid = b.empid 
+where b.bonus < 1000  or b.bonus is null;
