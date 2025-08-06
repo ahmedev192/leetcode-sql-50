37089ffd4ebec16e19e515662ab0cd7b68c4c5b8
Ahmed Mahmoud <ahmedev192@gmail.com>
2025-08-06 11:44:43 +0300

    Project Employees I

--- 23. Project Employees I.sql
+++ 23. Project Employees I.sql
@@ -1,2 +1,8 @@
 -- 23. Project Employees I
--- Question goes here.
+select
+p.project_id ,
+round( sum(e.experience_years  * 1.00) / count(*) , 2) as average_years
+from project p  
+left join employee e 
+on p.employee_id = e.employee_id 
+group by p.project_id  ;
